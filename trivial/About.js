cd5b38240527aa6ace32a51f7a8aaac725006e0e
Mike Taylor <mike@indexdata.com>
2016-06-09 12:52:17 +0100

    greetingParams now contains two elements: greeting and name. Both can be updated in the Web form, and together they constitute a record that the mutators act on.

--- trivial/About.js
+++ trivial/About.js
@@ -3,24 +3,29 @@ import { connect } from 'stripes-connect';
 class About extends Component {
   static dataQuery = { greetingParams: {} };
   componentWillMount() {
-    this.props.mutator.greetingParams.replace({ name: 'Kurt' });
+    this.props.mutator.greetingParams.replace({ greeting: 'Hi', name: 'Kurt' });
   }
   handleSubmit(e) {
-    this.props.mutator.greetingParams.replace({ name: e.target.firstChild.value });
+    let replacement = { greeting: document.getElementById('g').value,
+                        name: document.getElementById('n').value };
+    console.log(replacement);
+    this.props.mutator.greetingParams.replace(replacement);
   }
   render() {
     console.log('RENDR ABOUT');
     console.log(this.props);
     let greeting;
     if (this.props.greetingParams) {
-      greeting = <h3>Hi {this.props.greetingParams.name}</h3>
+      greeting = <h3>[{this.props.greetingParams.greeting}] [{this.props.greetingParams.name}]</h3>
     } else {
       greeting = <h3>No one here :(</h3>
     }
     return <div>
         {greeting}
         <form ref='form' onSubmit={this.handleSubmit.bind(this)}>
-          <input type='text' /><button type="submit">Greet</button>
+          Greeting: <input id='g' type='text' />
+          Person: <input id='n' type='text' />
+          <button type="submit">Greet</button>
         </form>
       </div>
   }
