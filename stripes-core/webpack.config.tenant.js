394614aad79558ac2e6ff5fc6de9d7f54050c703
Wolfram Schneider <wosch@indexdata.com>
2016-07-21 12:28:26 +0200

    build webpack for tenant
      npm run build:tenant

---
+++ stripes-core/webpack.config.tenant.js
@@ -0,0 +1,21 @@
+// Base Webpack configuration for building Stripes at the command line,
+// including Stripes configuration.
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  stripesLoader: {
+    okapi: { 'url':'http://localhost:9130' },
+    modules: {
+      '@stripes-experiments/trivial': {},
+      '@stripes-experiments/trivial-wosch': {},
+      '@stripes-experiments/trivial-okapi': {}
+    }
+  }
+};
