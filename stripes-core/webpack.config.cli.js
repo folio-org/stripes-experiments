d9144ab44d3371dbb5cd8b2ab3aaa7ac49c98caa
Wolfram Schneider <wosch@FreeBSD.org>
2016-07-22 15:34:50 +0000

    Revert "node.js version 6.x is required"
    This reverts commit f90e067f2f2c9b207c1d916831d81e0cad3d14c3.

--- stripes-core/webpack.config.cli.js
+++ stripes-core/webpack.config.cli.js
@@ -13,9 +13,8 @@ module.exports = {
   stripesLoader: {
     okapi: { 'url':'http://localhost:9130' },
     modules: {
-      // '@stripes-experiments/trivial': {},
-      '@stripes-experiments/trivial-wosch': {},
-      //'@stripes-experiments/trivial-okapi': {}
+      '@stripes-experiments/trivial': {},
+      '@stripes-experiments/trivial-okapi': {}
     }
   }
 };
