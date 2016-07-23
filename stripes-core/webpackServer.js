3328594f9af43b347fa9b2a18aa44da37fc6719d
Wolfram Schneider <wosch@indexdata.dk>
2016-07-23 19:09:31 +0000

    return URL as JSON object, in sync mode

--- stripes-core/webpackServer.js
+++ stripes-core/webpackServer.js
@@ -5,22 +5,28 @@ var port = 3030;
 var asyncblock = require('asyncblock');
 var exec = require('child_process').exec;
 
+var cache = {};
+
 app.get('/', function (req, res) {
   res.send("Please use http://localhost:" + port + "/bundle?url=module1&url=module2 ...\n");
 });
 
 app.get('/bundle', function (req, res) {
-  res.send("get bundle for " + req.query.url);
-  
+  var tenant = req.query.tenant
+
+  var id = [];
+  id.push(tenant);
+  id.push(req.query.url);
+  var id_tag = id.join("|");
   
   asyncblock(function (flow) {
     // exec('node -v', flow.add());
-    var command = 'env ui_url="' + req.query.url.join(" ");
+    var command = 'env tenant="' + tenant + '"' + ' ui_url="' + req.query.url.join(" ");
     command += '" ./bin/tenant-bundle.sh';
     
     exec(command, flow.add());
     
-    console.log('Run build, may take 1-2 minutes');
+    console.log('Run build, may take 1-2 minutes, tenant ' + tenant);
     console.log(req.query.url)
     
     result = flow.wait();
@@ -28,6 +34,14 @@ app.get('/bundle', function (req, res) {
 
     // Some other jobs
     console.log('More results like if it were sync...');
+    cache[id_tag] = result;
+
+    var lines = result.split("\n");
+    lines.pop();
+    var aws_url = { url: lines.pop() };
+    
+    // res.send("get bundle for tenant " + tenant + " " + req.query.url + result);
+    res.send(JSON.stringify(aws_url));
   });
 
 } );
