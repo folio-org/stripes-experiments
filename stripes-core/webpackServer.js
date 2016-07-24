a695211e8a78bec9a71b08413c9db7488450e842
Wolfram Schneider <wosch@indexdata.dk>
2016-07-24 11:16:21 +0000

    validate tenant/url paramters, and throw a better error message in case of failure

--- stripes-core/webpackServer.js
+++ stripes-core/webpackServer.js
@@ -6,6 +6,7 @@ var asyncblock = require('asyncblock');
 var exec = require('child_process').exec;
 
 var cache = {};
+var error = {}; // global error object
 
 app.get('/', function (req, res) {
   res.send("Please use http://localhost:" + port + "/bundle?tenant=tenant&url=module1&url=module2 ...\n");
@@ -13,15 +14,32 @@ app.get('/', function (req, res) {
 
 app.get('/bundle', function (req, res) {
   var tenant = req.query.tenant
+  if (typeof tenant == 'undefined' || tenant == '') {
+    return res.send(JSON.stringify({status: 503, message: 'missing tenant parameter' }));
+  }
+  if (!tenant.match(/^[[\w\-]+$/)) {
+    return res.send(JSON.stringify({status: 503, message: 'wrong tenant parameter [A-Za-z0-9-]+' }));
+  }
 
-  var id = [];
-  id.push(tenant);
-  id.push(req.query.url);
-  var id_tag = id.join("|");
+  //var id = [];
+  //id.push(tenant);
+  //id.push(req.query.url);
+  //var id_tag = id.join("|");
+  
+  var ui_url;
+  if (typeof req.query.url == 'object') {
+    ui_url = req.query.url.join(" ");
+  } else if (typeof req.query.url == 'string') {
+    ui_url = req.query.url;
+  } else {
+    return res.send(JSON.stringify({status: 503, message: 'missing url parameter' }));
+  }
+  
+  
   
   asyncblock(function (flow) {
     // exec('node -v', flow.add());
-    var command = 'env stripes_tenant="' + tenant + '"' + ' ui_url="' + req.query.url.join(" ");
+    var command = 'env stripes_tenant="' + tenant + '"' + ' ui_url="' + ui_url;
     command += '" ./bin/tenant-bundle.sh';
     
     exec(command, flow.add());
@@ -38,7 +56,7 @@ app.get('/bundle', function (req, res) {
 
     var lines = result.split("\n");
     lines.pop();
-    var aws_url = { url: lines.pop() };
+    var aws_url = { status: 200, url: lines.pop() };
     
     // res.send("get bundle for tenant " + tenant + " " + req.query.url + result);
     res.send(JSON.stringify(aws_url));
