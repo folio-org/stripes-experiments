cee65097c56dccb21ec5c450e5cb6a3a34a45ea3
Wolfram Schneider <wosch@indexdata.dk>
2016-08-02 22:32:54 +0200

    rewritten to support env arguments, e.g.
    tenant=test module=trivial2 ./ui-deploy.sh

--- bin/ui-deploy.sh
+++ bin/ui-deploy.sh
@@ -1,46 +1,58 @@
 #!/bin/sh
 
+: ${tenant="our"}
+: ${module="trivial"}
+
+module_json=$(mktemp)
+tenant_json=$(mktemp)
+
 # trivial module
-cat > /tmp/ui-module-trivial.json <<END
+cat > $module_json <<END
 {
-  "id" : "trivial",
-  "name" : "trivial",
+  "id" : "$module",
+  "name" : "$module",
   "uiDescriptor" : {
-     "npm" : "trivial"
+     "npm" : "$module"
   }
 }
 END
+
 curl -w '\n' -X POST -D - \
   -H "Content-type: application/json" \
-  -d @/tmp/ui-module-trivial.json  \
+  -d @$module_json  \
   http://localhost:9130/_/proxy/modules
 
 # same 1st tenant as manual
-cat > /tmp/tenant1.json <<END
+cat > $tenant_json <<END
 {
-  "id" : "our",
-  "name" : "our library",
-  "description" : "Our Own Library"
+  "id" : "$tenant",
+  "name" : "$tenant library",
+  "description" : "$tenant Library"
 }
 END
+
 curl -w '\n' -X POST -D - \
   -H "Content-type: application/json" \
-  -d @/tmp/tenant1.json  \
+  -d @$tenant_json \
   http://localhost:9130/_/proxy/tenants
 
 # Enable tenant
-cat > /tmp/enabletenant3.json <<END
+tenant_enable_json=$(mktemp)
+cat > $tenant_enable_json <<END
 {
-  "id" : "trivial"
+  "id" : "$module"
 }
 END
+
 curl -w '\n' -X POST -D - \
   -H "Content-type: application/json" \
-  -d @/tmp/enabletenant3.json  \
-  http://localhost:9130/_/proxy/tenants/our/modules
+  -d @$tenant_enable_json  \
+  http://localhost:9130/_/proxy/tenants/$tenant/modules
 
 # get list of enabled modules for tenant
-curl -w '\n' -D - http://localhost:9130/_/proxy/tenants/our/modules
+curl -w '\n' -D - http://localhost:9130/_/proxy/tenants/$tenant/modules
 
 # get full info for trivial (repeat for each one returned above)
-curl -w '\n' -D - http://localhost:9130/_/proxy/modules/trivial
+curl -w '\n' -D - http://localhost:9130/_/proxy/modules/$module
+
+rm -f $module_json $tenant_json $tenant_enable_json
