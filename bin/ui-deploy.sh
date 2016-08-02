10b45e1cf4c09dc9892710c02d890f96b12a0c36
Wolfram Schneider <wosch@indexdata.dk>
2016-08-02 22:47:09 +0200

    enable to add several modules in one step
    tenant=test module="trivial trivial-okapi" ./ui-deploy.sh

--- bin/ui-deploy.sh
+++ bin/ui-deploy.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
 
 : ${tenant="our"}
-: ${module="trivial"}
+: ${modules="trivial trivial-okapi"}
 
 module_json=$(mktemp)
 tenant_json=$(mktemp)
 
+########################################
+# same 1st tenant as manual
+#
+cat > $tenant_json <<END
+{
+  "id" : "$tenant",
+  "name" : "$tenant library",
+  "description" : "$tenant Library"
+}
+END
+
+curl -w '\n' -X POST -D - \
+  -H "Content-type: application/json" \
+  -d @$tenant_json \
+  http://localhost:9130/_/proxy/tenants
+
+
+
+########################################
+# modules
+#
+for module in $modules
+do
+    
 # trivial module
 cat > $module_json <<END
 {
@@ -22,20 +46,6 @@ curl -w '\n' -X POST -D - \
   -d @$module_json  \
   http://localhost:9130/_/proxy/modules
 
-# same 1st tenant as manual
-cat > $tenant_json <<END
-{
-  "id" : "$tenant",
-  "name" : "$tenant library",
-  "description" : "$tenant Library"
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @$tenant_json \
-  http://localhost:9130/_/proxy/tenants
-
 # Enable tenant
 tenant_enable_json=$(mktemp)
 cat > $tenant_enable_json <<END
@@ -49,10 +59,11 @@ curl -w '\n' -X POST -D - \
   -d @$tenant_enable_json  \
   http://localhost:9130/_/proxy/tenants/$tenant/modules
 
-# get list of enabled modules for tenant
-curl -w '\n' -D - http://localhost:9130/_/proxy/tenants/$tenant/modules
-
 # get full info for trivial (repeat for each one returned above)
 curl -w '\n' -D - http://localhost:9130/_/proxy/modules/$module
+done
+
+# get list of enabled modules for tenant
+curl -w '\n' -D - http://localhost:9130/_/proxy/tenants/$tenant/modules
 
 rm -f $module_json $tenant_json $tenant_enable_json
