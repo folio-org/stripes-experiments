74ded40cff1fa688538295805cdd3bd297b39610
Wolfram Schneider <wosch@indexdata.dk>
2016-08-04 17:49:47 +0200

    fetch tenant module list from okapi service, and create a bundle
    $ node stripes-core/uiDescriptor.js test
    http://s3.amazonaws.com/folio-ui-bundle/tenant/test-1470325294/index.html

--- stripes-core/uiDescriptor.js
+++ stripes-core/uiDescriptor.js
@@ -1,13 +1,70 @@
 var request = require('request');
 
 var args = process.argv.slice(2);
-var url = args[0];
+var tenant = args[0] || "test";
 
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
-  } else {
-    console.log("HTTP status for " + url + " " + response.statusCode);
+// return a simple module list 
+function modules_list(modules) {
+  var list = [];
+  for (var i = 0; i < modules.length; i++) {
+    list.push(modules[i].id);
   }
-})
+  
+  return list;
+}
+
+function get_module_list(tenant, callback) {
+  var url = "http://localhost:9130/_/proxy/tenants/" + tenant + "/modules";
+  
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+       var modules = JSON.parse(body);
+       var list = modules_list(modules);
+       callback(tenant, list);
+       
+    } else {
+      console.log("HTTP status for " + url + " " + response.statusCode);
+    }
+  })
+}
+
+function body_data(modules) {
+  var obj = { url: modules };
+  
+  return JSON.stringify(obj);
+};
+
+function webpack_service(tenant, modules) {
+  var body = body_data(modules);
+  var url = 'http://localhost:3030/bundle';
+  // Set the headers
+  
+  var headers = {
+      'X-Okapi-Tenant-Id': tenant,
+      'User-Agent':       'Folio Agent/0.0.1',
+      'Content-Type':     'application/json'
+  }
+   
+  // Configure the request
+  var options = {
+      method: 'POST',
+      url: url,
+      headers: headers,
+      body: body
+  }
+
+  // console.log(options);
+  
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 201) {
+      console.log(response.headers.location)
+    } else {
+      console.log("HTTP status for " + url + " " + response.statusCode);
+    }
+  })
+}
+
+
+// http://localhost:9130/_/proxy/tenants/$tenant/modules
+get_module_list(tenant, webpack_service);
 
