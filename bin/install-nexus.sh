b80616008b58b8cdb446fe8a16a3ba3a9a82610f
Wolfram Schneider <wosch@indexdata.dk>
2016-08-11 13:09:37 +0200

    less verbose output

--- bin/install-nexus.sh
+++ bin/install-nexus.sh
@@ -15,7 +15,7 @@ mkdir -p stripes-core/node_modules && \
    cd stripes-core/node_modules/ && \
    rm -rf stripes-loader && \
    rm -rf stripes-loader-[0-9]* && \
-   wget https://s3.amazonaws.com/folio-ui-bundle/tarball/stripes-loader-0.0.0-nogit.tgz && \
+   wget -q https://s3.amazonaws.com/folio-ui-bundle/tarball/stripes-loader-0.0.0-nogit.tgz && \
    tar xfz stripes-loader-0.0.0-nogit.tgz && \
    sed -i.bak -e 's/^[ \t]*"stripes-loader"[ \t]*:.*//' ../package.json
 )
