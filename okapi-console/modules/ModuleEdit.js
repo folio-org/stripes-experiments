e437792d96d38a3ec1b2cd40ab1c2283f63a0b8f
Niels Erik G. Nielsen <nielserik@indexdata.com>
2016-08-29 23:24:07 +0200

    Implementing redux-form 6 FieldArrays for Module objects
      Styling messed up in the migration.
      Routing 'methods' array not done.

--- okapi-console/modules/ModuleEdit.js
+++ okapi-console/modules/ModuleEdit.js
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'stripes-connect';
 import ModuleForm from './ModuleForm';
-import TestForm from './reduxForm';
 import { removeEmpty } from '../utils/removeEmptyObjectsFromArrays';
+
 
 class ModuleEdit extends Component {
   static contextTypes = {
@@ -24,36 +24,16 @@ class ModuleEdit extends Component {
     this.context.router.push('/okapi-console/modules/list');
   }
 
-  sub(data) {
-    console.log("submitting data: ", data);
-  }
-
-  
   render() {
     let moduleid = this.props.params.moduleid;
     let modules = this.props.data['modules']
     let module = modules.find((module) =>  { return module.id === moduleid });
-    console.log("modules: ", modules);
-    console.log("moduleid: ", moduleid);
-    console.log("module: ", module);
-    console.log("module.provides: "+(module.provides ? module.provides.id : "no provides"));
-    let obj1 = Object.assign({}, module);
-    let obj2 = { id: "module-id", name: "patrons", provides: [{ "id" :"prov-id", "version" : "1.2.3"}] };
-
-    console.log("obj1 ", obj1);
-    console.log("obj2 ", obj2);
-    return <TestForm initialValues={obj1} onSubmit={this.sub.bind(this)} />
+    if (!module || !module.provides) {
+      return <div/>;
+    }
+    return <ModuleForm initialValues={module} onSubmit={this.update.bind(this)} cancelForm={this.cancel.bind(this)} />
   }
 }
-
-/*
-    return <TestForm initialValues={obj1} onSubmit={this.sub.bind(this)} />
-
-    return <ModuleForm onSubmit={this.sub.bind(this)} 
-                       cancelForm={this.cancel.bind(this)} 
-                       submitLabel='Save'
-                       initialValues={module} />
-*/
 
 export default connect(ModuleEdit, 'okapi-console');
 
--- okapi-console/modules/ModuleForm.js
+++ okapi-console/modules/ModuleForm.js
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { Grid, Container, Row, Col, Form, FormGroup, FormControl, ControlLabel, Input, Button, ButtonGroup, Glyphicon } from 'react-bootstrap';
 import {Field, FieldArray, reduxForm} from 'redux-form';
 import Deployments from './Deployments';
-
 
 class ModuleForm extends Component {
   static propTypes = {
@@ -18,77 +18,138 @@ class ModuleForm extends Component {
 
   render() {
     const {
-      array: { push },
       handleSubmit, 
       pristine,
       reset,
       submitting,
       cancelForm, 
-      submitLabel, 
-      disableFields,
+      submitLabel
     } = this.props;
 
-    const renderProvides = ({ fields }) => (
-      <ul>
-        <li>
-          <button type="button" onClick={() => fields.push({})}>Add Provides</button>
-        </li>
-        {fields.map((fld, index) =>
-          <li key={index}>
-            <button
-              type="button"
-              title="Remove Provision"
-              onClick={() => fields.remove(index)}/>
-            <h4>Provision #{index + 1}</h4>
-            <Field
-              name={`${fld}.id`}
-              type="text"
-              component="input"
-              placeholder="Module ID"/>
-            <br/>
-            <Field
-              name={`${fld}.version`}
-              type="text"
-              component="input"
-              placeholder="Version"/>
-          </li>
-        )}
-      </ul>
-    )
-
-
     return (
-      <div>
-        <Form inline>
-          <h3>{(submitLabel==='Add' ? 'Add ' :
-                 (submitLabel==='Save' ? 'Edit ' :
-                   (submitLabel === 'Delete' ? 'Delete ' : '') : '') : '')} module proxy</h3>
-          <Row>
-            <Col componentClass={ControlLabel} sm={2}>
-              Name
-            </Col>
-            <Col sm={10}>
-              <Field name="name" component="input" type='text' placeholder='Module name' />
-            </Col>
-          </Row>
-          <br/>
-          <FieldArray name="provides" component={renderProvides} />
-          <br/><br/>
-          <ButtonGroup className='pull-right'>
-            <Button type='submit' bsStyle='primary' disabled={submitting||(!disableFields&&pristine)} onClick={handleSubmit}>{submitLabel} module proxy</Button>
-            <Button type='reset' disabled={submitting||disableFields||pristine} onClick={reset}>Reset</Button>
-            <Button type='button' disabled={submitting} onClick={cancelForm}>{pristine? 'Go back' : 'Cancel'}</Button>
-          </ButtonGroup>
-        </Form>
-        <br/><br/>
-        
-      </div>
-    );
+      <Form inline>
+            <h3>{(submitLabel==='Add' ? 'Add ' :
+                   (submitLabel==='Save' ? 'Edit ' :
+                     (submitLabel === 'Delete' ? 'Delete ' : '') : '') : '')} module proxy</h3>
+            <Row>
+              <Col componentClass={ControlLabel} sm={2}>
+                Name
+              </Col>
+              <Col sm={10}>
+                <Field name="name" component="input" type='text' placeholder='Module name' />
+              </Col>
+            </Row>
+            <br/>
+            <FieldArray name="provides" component={renderProvides} />
+            <br/>
+            <FieldArray name="requires" component={renderRequires} />
+            <br/>
+            <FieldArray name="routingEntries" component={renderRoutingEntries} />
+            <ButtonGroup className='pull-right'>
+              <Button type='submit' bsStyle='primary' disabled={submitting||(pristine)} onClick={handleSubmit}>{submitLabel} module proxy</Button>
+              <Button type='reset' disabled={submitting||pristine} onClick={reset}>Reset</Button>
+              <Button type='button' disabled={submitting} onClick={cancelForm}>{pristine? 'Go back' : 'Cancel'}</Button>
+            </ButtonGroup>
+      </Form>)  
   }
 }
+
+const renderProvides = ({ fields }) => (
+  <ul>
+    <li>
+      <button type="button" onClick={() => fields.push({})}>Add Provides</button>
+    </li>
+    {fields.map((fld, index) =>
+      <li key={index}>
+        <button
+          type="button"
+          title="Remove Provision"
+          onClick={() => fields.remove(index)}/>
+        <h4>Provision #{index + 1}</h4>
+        <Field
+          name={`${fld}.id`}
+          type="text"
+          component="input"
+          placeholder="Module ID"/>
+        <br/>
+        <Field
+          name={`${fld}.version`}
+          type="text"
+          component="input"
+          placeholder="Version"/>
+      </li>
+    )}
+  </ul>
+)
+
+const renderRequires = ({ fields }) => (
+  <ul>
+    <li>
+      <button type="button" onClick={() => fields.push({})}>Add Requirements</button>
+    </li>
+    {fields.map((fld, index) =>
+      <li key={index}>
+        <button
+          type="button"
+          title="Remove requirement"
+          onClick={() => fields.remove(index)}/>
+        <h4>Requirement #{index + 1}</h4>
+        <Field
+          name={`${fld}.id`}
+          type="text"
+          component="input"
+          placeholder="Module ID"/>
+        <br/>
+        <Field
+          name={`${fld}.version`}
+          type="text"
+          component="input"
+          placeholder="Version"/>
+      </li>
+    )}
+  </ul>
+)
+
+const renderRoutingEntries = ({ fields }) => (
+  <ul>
+    <li>
+      <button type="button" onClick={() => fields.push({})}>Add Routing</button>
+    </li>
+    {fields.map((fld, index) =>
+      <li key={index}>
+        <button
+          type="button"
+          title="Remove routing"
+          onClick={() => fields.remove(index)}/>
+        <h4>Route #{index + 1}</h4>
+        <Field
+          name={`${fld}.path`}
+          type="text"
+          component="input"
+          placeholder="Path to module"/>
+        <br/>
+        <Field
+          name={`${fld}.level`}
+          type="text"
+          component="input"
+          placeholder="Level"/>
+        <br/>
+        <Field
+          name={`${fld}.type`}
+          type="text"
+          component="input"
+          placeholder="Request type"/>
+
+      </li>
+    )}
+  </ul>
+)
+
+
 
 export default reduxForm(
   {
     form: 'moduleForm'
   }
 )(ModuleForm);
+
