abf3685b4bc1d6d6b44927e6dfa0ef8bf54ba11e
Jason Skomorowski <jason@indexdata.com>
2016-10-17 10:10:11 -0400

    standalone-component: support new api, add linting

--- standalone-component/index.js
+++ standalone-component/index.js
@@ -1,48 +1,48 @@
 import 'babel-polyfill';
 import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
-import { combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { reducer as formReducer } from 'redux-form';
+import Router from 'react-router/HashRouter';
+import Match from 'react-router/Match';
 
 const ComponentToTest = require(COMPONENT_TO_TEST).default;
 
-let reducers = { form: formReducer };
+const reducers = { form: formReducer };
 const finalCreateStore = compose(
   applyMiddleware(thunk),
   applyMiddleware(createLogger()),
 )(createStore);
-const store = finalCreateStore(combineReducers({...reducers}));
-
+const store = finalCreateStore(combineReducers({ ...reducers }));
 
 class Root extends Component {
   addReducer = (key, reducer) => {
     if (reducers[key] === undefined) {
       reducers[key] = reducer;
-      this.props.store.replaceReducer(combineReducers({...reducers}));
+      this.props.store.replaceReducer(combineReducers({ ...reducers }));
       return true;
-    } else return false;
+    }
+    return false;
   }
 
   getChildContext() {
-    return {addReducer: this.addReducer.bind(this)};
+    return { addReducer: this.addReducer.bind(this) };
   }
 
   render() {
-    const { store } = this.props;
     return (
-      <Provider store={store}>
-        <ComponentToTest/>
-      </Provider>
+      <Provider store={this.props.store}><Router>
+        <Match pattern="/" component={ComponentToTest} />
+      </Router></Provider>
     );
   }
 }
 
 Root.childContextTypes = {
-  addReducer: PropTypes.func
+  addReducer: PropTypes.func,
 };
 
 render(
