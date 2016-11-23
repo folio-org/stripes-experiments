8c0cd7503af795d7ee9e273b8dfabd197dfb7cd3
Mike Taylor <mike@indexdata.com>
2016-11-23 11:45:31 +0000

    Get FilterPane from lib directory. (I moved it down because we are going to need local copies of other components as well, most immediately FilterPaneSearch.)

--- users/Users.js
+++ users/Users.js
@@ -12,7 +12,7 @@ import KeyValue from '@folio/stripes-components/lib/KeyValue'
 import {Row, Col} from 'react-bootstrap'
 import TextField from '@folio/stripes-components/lib/TextField'
 import Checkbox from '@folio/stripes-components/lib/Checkbox'
-import FilterPane from './FilterPane/index' // I don't know why simply './FilterPane' doesn't work.
+import FilterPane from './lib/FilterPane'
 import Select from '@folio/stripes-components/lib/Select'
 
 class Users extends React.Component{
