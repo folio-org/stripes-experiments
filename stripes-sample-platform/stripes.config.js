643045834778bdd6bfe019716b4defc47b22e06d
Mike Taylor <mike@indexdata.com>
2017-02-13 13:52:32 +0000

    Add configuration to turn off authentication. Allows demo to show Hello World module.

--- stripes-sample-platform/stripes.config.js
+++ stripes-sample-platform/stripes.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+  config: { reduxLog: true, disableAuth: true },
   modules: {
     '@folio-sample-modules/trivial': {},
     '@folio-sample-modules/trivial-okapi': {},
