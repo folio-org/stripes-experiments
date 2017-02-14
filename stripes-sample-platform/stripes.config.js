b8d7510de8b8e0c472467493cb1851babfa8f6e5
Jason Skomorowski <jason@indexdata.com>
2017-02-14 12:10:39 -0500

    Delete sample platform (now in own repo)

--- stripes-sample-platform/stripes.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
-  config: { reduxLog: true, disableAuth: true },
-  modules: {
-    '@folio-sample-modules/trivial': {},
-    '@folio-sample-modules/trivial-okapi': {},
-    '@folio-sample-modules/users': {},
-  }
-};
